0ad39bd1b8b8e79885421e9f3502e81b616a9443
Chris Huys <chrishuys@live.com>
2017-02-21 11:31:48 +0100

    ParkingBonMVVM

--- ParkingBonMVVM/ParkingBonMVVM/View/ParkingBonVIew.xaml.cs
+++ ParkingBonMVVM/ParkingBonMVVM/View/ParkingBonVIew.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ParkingBon.View
+namespace ParkingBonMVVM.View
 {
     /// <summary>
     /// Interaction logic for ParkingBonWindow.xaml
